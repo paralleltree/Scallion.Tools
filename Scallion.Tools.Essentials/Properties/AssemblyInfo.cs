e2b31ec8f7ca7591e10be2d6055e8e929f7785f5
Paralleltree <paralleltree@outlook.com>
2016-06-08 22:00:24 +0900

    Add a base project

---
+++ Scallion.Tools.Essentials/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Scallion.Tools.Essentials")]
+[assembly: AssemblyDescription("A helper assembly for running as console application")]
+[assembly: AssemblyCompany("paltee.net")]
+[assembly: AssemblyProduct("Scallion.Tools.Essentials")]
+[assembly: AssemblyCopyright("Copyright (C) 2016 Paralleltree")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9be64c27-cae3-444c-84e9-ea919ab26168")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]

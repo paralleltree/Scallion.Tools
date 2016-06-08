bf4999f1a21e18d4b7d03c4ce9925a072d2ca8f7
Paralleltree <paralleltree@outlook.com>
2016-06-09 00:18:03 +0900

    Implement base components

---
+++ Scallion.Tools.Essentials/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Scallion.Tools.Essentials
+{
+    /// <summary>
+    /// Contains several extension methods.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Retrieves a custom attribute applied to the assembly.
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute to retrieve</typeparam>
+        /// <param name="src">An object to search for the attribute</param>
+        /// <returns>
+        /// A reference to the attribute of type <typeparamref name="T"/> that is applied to <paramref name="src"/>,
+        /// or null if there is no such attribute
+        /// </returns>
+        public static T GetCustomAttribute<T>(this Assembly src) where T : Attribute
+        {
+            return (T)Attribute.GetCustomAttribute(src, typeof(T));
+        }
+    }
+}

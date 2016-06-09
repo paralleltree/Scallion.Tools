fde908005dc00793b935dc3ca1b745ee8cb8b797
Paralleltree <paralleltree@outlook.com>
2016-06-09 20:35:17 +0900

    Add vmdscale project

---
+++ vmdscale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scallion.Tools.VmdScale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

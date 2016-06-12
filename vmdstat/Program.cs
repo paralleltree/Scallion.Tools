15cd8d4f0f05263fd18f7ad4d0c4afe7b0953536
Paralleltree <paralleltree@outlook.com>
2016-06-13 00:26:13 +0900

    Add vmdstat project

---
+++ vmdstat/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scallion.Tools.VmdStat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ vmdstat/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("vmdstat")]
+[assembly: AssemblyDescription("display vmd file statistics")]
+[assembly: AssemblyCompany("paltee.net")]
+[assembly: AssemblyProduct("vmdstat")]
+[assembly: AssemblyCopyright("Copyright (C) 2016 Paralleltree")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("55e45031-73de-40db-a801-73530a7ba1a7")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
